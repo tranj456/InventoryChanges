6e6acca1316b1aa15d3c5e6546785c8e8eeab23a
dluman <dluman@allegheny.edu>
2022-06-27 00:55:08 -0400

    Committing latest version of couchsurf; previous version missing content

--- libs/couchsurf/src/couchsurf.py
+++ libs/couchsurf/src/couchsurf.py
@@ -1,3 +1,4 @@
+import re
 import json
 import requests
 
@@ -8,20 +9,63 @@ CONFIG = dotenv_values('.env')
 HEADERS = {
     'accept': 'application/json',
     'content-type': 'application/json',
-    'referer': 'https://fauxton.cdr.theterm.world'
+    'referer': f'https://{CONFIG["GOVERNOR_HOST"]}'
 }
 
-def get_request(view_path="/_design/latest/_view/latest-poll"):
-    response = requests.get(f'https://{CONFIG["GOVERNOR_URI"]}{view_path}',
-    headers=HEADERS,
+def get_request(view_path="latest-poll", **kwargs):
+    params = None
+    if kwargs:
+        params = {
+            "keys": json.dumps([value for value in kwargs.values()])
+        }
+    response = requests.get(
+        f'https://{CONFIG["GOVERNOR_URI"]}/_design/latest/_view/{view_path}',
+        headers = HEADERS,
+        params = params
     )
     return response.text
 
-def post_request(doc):
+def query_request(**kwargs):
+    """
+        Example:
+        couchsurf.query_request(
+            scope={"op":"EQUALS", "arg":"global"},
+            flag ={"op":"EQUALS", "arg":"active"}
+        )
+    """
+    operators = {
+        "LESS THAN": "$lt",
+        "GREATER THAN": "$gt",
+        "EQUALS":"$eq",
+        "LIKE": "$regex"
+    }
+    for param in kwargs:
+        op = kwargs[param]["op"]
+        arg = kwargs[param]["arg"]
+        if op == "LIKE": arg = f"(*UTF8)(?i){arg}"
+        kwargs[param] = {
+            operators[op]:f"{arg}"
+        }
+    query = {
+        "selector":kwargs
+    }
+    result = post_request(query, "_find")
+    return result
+
+def post_request(doc, op=""):
     response = requests.post(
-        f'https://{CONFIG["GOVERNOR_URI"]}',
+        f'https://{CONFIG["GOVERNOR_URI"]}/{op}',
         headers=HEADERS,
         data=json.dumps(doc)
     )
     confirmation = json.loads(response.text)
-    print(f'A new doc has been posted at id: {confirmation["id"]}')
+    return confirmation
+
+def put_request(doc_id, updated_doc):
+    response = requests.put(
+        f'https://{CONFIG["GOVERNOR_URI"]}/{doc_id}',
+        headers=HEADERS,
+        data=json.dumps(updated_doc)
+    )
+    confirmation = json.loads(response.text)
+    return confirmation
