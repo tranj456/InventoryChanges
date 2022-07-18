68522d9e0d5157be7d7e949309be640ae427e335
dluman <dluman@allegheny.edu>
2022-07-18 08:24:53 -0400

    For later

--- libs/inventory/src/__init__.py
+++ libs/inventory/src/__init__.py
@@ -6,4 +6,22 @@ from .Inventory import *
 args = sys.argv[1:]
 
 if args:
-  print(args)
+  import importlib
+  import shutil
+
+  file = args[0]
+
+  # TODO: Migrate the below to separate functions
+  #       to test each condition separately and raise
+  #       specific errors
+
+  try:
+    name, ext = file.split(".")
+    if not ext == "py":
+      raise
+    importlib.import_module(name)
+  except:
+    print("Not a Python file.")
+    exit()
+
+  shutil.copy(file, f"~/.inv/{file}")
