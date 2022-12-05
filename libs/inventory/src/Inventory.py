085dcd61865bdb7cd1aaad14d1ac7759a17b8a1e
charlesfgibson <gibson01@allegheny.edu>
2022-12-05 10:38:41 -0500

    fixed consumability and __str__ bug

--- libs/inventory/src/Inventory.py
+++ libs/inventory/src/Inventory.py
@@ -26,140 +26,140 @@ sys.path.append(
 
 class Acquire:
 
-  import shutil
-  import importlib
-
-  def __init__(self, filename):
-    self.filename = filename
-    self.validate()
-    self.move()
-    self.add()
-
-  def is_box(self, item) -> bool:
-    self.box = "BoxSpec" in dir(item)
-
-  def validate(self):
-    import importlib
-    try:
-      self.name, self.ext = self.filename.split(".")
-      if not self.ext == "py":
-        raise
-      obj = importlib.import_module(self.name)
-      getattr(obj, self.name)().use
-      self.is_box(obj)
-    except Exception as e:
-      print("Not a valid item file")
-      exit()
-
-  def move(self):
     import shutil
-    try:
-      path = os.path.expanduser(
-        f'{Config.values["INV_PATH"]}/{self.filename}'
-      )
-      if not self.box:
-        shutil.copy(self.filename, path)
-    except Exception as e:
-      print(f"Couldn't acquire {self.name}")
-      exit()
+    import importlib
 
-  def add(self):
-    try:
-      list.add(self.name)
-    except Exception as e:
-      print(f"Couldn't acquire {self.name}")
-      exit()
+    def __init__(self, filename):
+        self.filename = filename
+        self.validate()
+        self.move()
+        self.add()
+
+    def is_box(self, item) -> bool:
+        self.box = "BoxSpec" in dir(item)
+
+    def validate(self):
+        import importlib
+        try:
+            self.name, self.ext = self.filename.split(".")
+            if not self.ext == "py":
+                raise
+            obj = importlib.import_module(self.name)
+            getattr(obj, self.name)().use
+            self.is_box(obj)
+        except Exception as e:
+            print("Not a valid item file")
+            exit()
+
+    def move(self):
+        import shutil
+        try:
+            path = os.path.expanduser(
+                f'{Config.values["INV_PATH"]}/{self.filename}'
+            )
+            if not self.box:
+                shutil.copy(self.filename, path)
+        except Exception as e:
+            print(f"Couldn't acquire {self.name}")
+            exit()
+
+    def add(self):
+        try:
+            list.add(self.name)
+        except Exception as e:
+            print(f"Couldn't acquire {self.name}")
+            exit()
 
 class List:
 
   # File operations
 
-  def __init__(self):
-    self.inventory = {}
-    self.path = os.path.expanduser(f'{Config.values["INV_PATH"]}')
-    try:
-      fh = open(
-        os.path.expanduser(PATH),
-        "r+"
-      )
-      self.inventory = json.load(fh)
-      fh.close()
-    except: pass
+    def __init__(self):
+        self.inventory = {}
+        self.path = os.path.expanduser(f'{Config.values["INV_PATH"]}')
+        try:
+            fh = open(
+                os.path.expanduser(PATH),
+                "r+"
+              )
+            self.inventory = json.load(fh)
+            fh.close()
+        except: pass
 
-  def write(self) -> None:
-    self.empties()
-    with open(
-      os.path.expanduser(PATH),
-      "w"
-    ) as fh:
-      json.dump(self.inventory, fh)
+    def write(self) -> None:
+        self.empties()
+        with open(
+            os.path.expanduser(PATH),
+            "w"
+        ) as fh:
+            json.dump(self.inventory, fh)
 
   # Representation
 
-  def __str__(self) -> str:
-    return json.dumps(self.inventory)
+    def __str__(self) -> str:
+        return json.dumps(self.inventory)
 
   # Add/remove items
 
-  def add(self, item: str, number: int = 1) -> None:
-    if item in self.inventory:
-      self.inventory[item]["quantity"] += number
-    else:
-      self.inventory[item] = {
-        "quantity": number,
-        "filename": f"{item}.py"
-      }
-    self.write()
+    def add(self, item: str, number: int = 1) -> None:
+        if item in self.inventory:
+            self.inventory[item]["quantity"] += number
+        else:
+            self.inventory[item] = {
+                "quantity": number,
+                "filename": f"{item}.py"
+            }
+        self.write()
 
-  def remove(self, item: str, number: int = -1) -> None:
-    self.add(item, number)
+    def remove(self, item: str, number: int = -1) -> None:
+        self.add(item, number)
 
   # Automatically remove empty or negative quantity items
 
-  def empties(self) -> None:
-    deletes = []
-    for item in self.inventory:
-      if self.inventory[item]["quantity"] <= 0:
-        deletes.append(item)
-    for item in deletes:
-      del self.inventory[item]
+    def empties(self) -> None:
+        deletes = []
+        for item in self.inventory:
+            if self.inventory[item]["quantity"] <= 0:
+                deletes.append(item)
+        for item in deletes:
+            del self.inventory[item]
 
   # Create a nice(r) display
 
-  def display(self):
-    table = Table(title=f"{os.getenv('LOGNAME')}'s inventory")
+    def display(self):
+        table = Table(title=f"{os.getenv('LOGNAME')}'s inventory")
 
-    table.add_column("Item name")
-    table.add_column("Item count")
-    table.add_column("Item file")
-    table.add_column("Consumable")
-    for item in self.inventory:
-      table.add_row(
-        item,
-        str(self.inventory[item]["quantity"]),
-        self.inventory[item]["filename"],
-        str(self.determine_consumable(item))
-      )
+        table.add_column("Item name")
+        table.add_column("Item count")
+        table.add_column("Item file")
+        table.add_column("Consumable")
+        for item in self.inventory:
+            table.add_row(
+                item,
+                str(self.inventory[item]["quantity"]),
+                self.inventory[item]["filename"],
+                str(self.determine_consumable(item))
+            )
 
-    console = Console()
-    console.print(table)
+        console = Console()
+        console.print(table)
   
   # Returns a boolean whether the item object is a consumable
     
-  def determine_consumable(self, item: str) -> bool:
+    def determine_consumable(self, item: str) -> bool:
     
-    from importlib import import_module   
-    try:
-      item_file = import_module(f"{item}")
-    except ModuleNotFoundError:
-      #print(f"You don't seem to have any {item}.")
-      return
-    try:
-      instance = getattr(item_file, item)()
-    except:
-      print(f"{item} doesn't seem to be a valid object.")
-      return 
-    return instace.consumable
+        from importlib import import_module   
+        try:
+            item_file = import_module(f"{item}")
+        except ModuleNotFoundError:
+          #print(f"You don't seem to have any {item}.")
+            return
+        try:
+            instance = getattr(item_file, item)()
+        except:
+            print(f"{item} doesn't seem to be a valid object.")
+            return 
+        return instance.consumable
 
 # Create instances to use as shorthand
 # I thought this was a bad idea, but this
@@ -208,6 +208,13 @@ class Items:
             print(f"You don't seem to have any {item}.")
             return
 
+        # Reflect the class
+        try:
+            instance = getattr(item_file, item)()
+        except:
+            print(f"{item} doesn't seem to be a valid object.")
+            return
+        
     # Test type of item; remove if ItemSpec
         try:
             box = self.is_box(item_file)
@@ -215,7 +222,10 @@ class Items:
             if fixture or box:
                 raise IsFixture(item)
             number = self.list[item]["quantity"]
-            list.add(item, -1)
+            
+            # only decreases quantity if it is a consumable
+            if instance.consumable:
+                list.add(item, -1)
             if number <= 0:
                 raise OutOfError(item)
         except (KeyError, OutOfError) as e:
@@ -223,20 +233,13 @@ class Items:
             return
         except IsFixture as e: pass
 
-    # Reflect the class
-        try:
-            instance = getattr(item_file, item)()
-        except:
-            print(f"{item} doesn't seem to be a valid object.")
-            return
-
     # To or not to remove; that is the question
 
     # edited so now the item can be used multiple times while still functioning
-    if instance.consumable and number <= 0:
-      try:
-        list.remove(item)
-      except: pass
+        if instance.consumable and number <= 0:
+            try:
+                list.remove(item)
+            except: pass
 
       # File now removes at the Spec level
       # os.remove(
@@ -246,7 +249,7 @@ class Items:
     # Return the result or inbuilt use method
         if type(instance).__str__ is not object.__str__:
             instance.use(**instance.actions)
-            print(f"{instance}")
+            # print(f"{instance}")
         else:
             return instance.use(**instance.actions)
 
